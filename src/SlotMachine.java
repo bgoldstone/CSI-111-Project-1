d61edea649e902e27964495be4d095ae101d4f71
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-03-02 18:25:01 -0500

    Initial Commit

---
+++ src/SlotMachine.java
@@ -0,0 +1,11 @@
+/**
+ * Programmer: Ben Goldstone
+ * Date: 3/2/2021
+ * Professor: Dr. Joseph Helsing
+ * Description: A program that Emulates a Slot Machine.
+ */
+public class SlotMachine {
+    public static void main(String[] args) {
+
+    }
+}
