5d7c45bcfe254fa55dc664d43958b7025f24b73e
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-03-02 18:49:08 -0500

    Added Welcome messages and Initialized Public Variables

--- src/SlotMachine.java
+++ src/SlotMachine.java
@@ -5,7 +5,25 @@
  * Description: A program that Emulates a Slot Machine.
  */
 public class SlotMachine {
+    //public variables
+    public double playerBalance = 100;
+    public double machineBalance = 0;
+    public final String[] WORDS = {"Computer", "Science", "Java", "Hello", "World", "Professor", "Helsing"};
+    public String[][] reels = new String[3][3];
+    public int defaultBet = 1;
+
     public static void main(String[] args) {
+        welcome();
+    }
+    public static void welcome(){
+        System.out.println("#".repeat(32));
+        System.out.println("##   Welcome to the Casino!   ##");
+        System.out.println("##                            ##");
+        System.out.println("## Default Starting bet is $1 ##");
+        System.out.println("##                            ##");
+        System.out.println("##  Player starts with $100   ##");
+        System.out.println("##                            ##");
+        System.out.println("#".repeat(32));
 
     }
 }
