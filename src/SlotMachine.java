120b037e5499f34818fde55f453160392557c983
Benjamin Goldstone <bgoldstone1@gmail.com>
2021-03-03 19:30:00 -0500

    changed global variables to local variables

--- src/SlotMachine.java
+++ src/SlotMachine.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -8,15 +9,14 @@ import java.util.Scanner;
  */
 public class SlotMachine {
     //Global Variables
-    public static Scanner scan = new Scanner(System.in);
-    public static double playerBalance = 100.00;
-    public static double machineBalance = 0.00;
-    public static double betAmount = 1.00;
-    public static double winnings = 0.00;
 
     public static void main(String[] args) {
+        //initializes variables
+        boolean flag = true;
         welcome();
-        menu();
+        while (flag) {
+            flag = menu();
+        }
     }
 
     /**
@@ -36,43 +36,64 @@ public class SlotMachine {
 
     /**
      * Prompts user for choice of what action to do
+     *
+     * @return boolean value to see if machine is still running
      */
-    public static void menu() {
-        //initializes variables
-        boolean flag = true;
+    public static boolean menu() {
+        //initializes local variables
+        boolean keepGoing = true;
+        Scanner scan = new Scanner(System.in);
+        double playerBalance = 100.00;
+        double machineBalance = 0.00;
+        double betAmount = 1.00;
+        double winnings = 0.00;
 
-        //slot machine loop
-        while (flag) {
-            //tells user balances
-            printStats();
+        //tells user balances
+        printStats(playerBalance, machineBalance);
 
-            //prompts user for menu option
-            System.out.print("""
-                    Please select an option:
-                    1. Add Money to the Machine\s""");
-            System.out.printf("\n2. Change bet amount (current bet is $%.2f and the default is $1.00)\n", betAmount);
-            System.out.print("""
-                    3. Play the game
-                    4. Leave the Machine and pay out all of your winnings
-                    Select(1,2,3,4):\s""");
-            int choice = scan.nextInt();
-            System.out.println();
+        //prompts user for menu option
+        System.out.print("""
+                Please select an option:
+                1. Add Money to the Machine\s""");
+        System.out.printf("\n2. Change bet amount (current bet is $%.2f and the default is $1.00)\n", betAmount);
+        System.out.print("""
+                3. Play the game
+                4. Leave the Machine and pay out all of your winnings
+                Select(1,2,3,4):\s""");
+        int choice = scan.nextInt();
+        System.out.println();
 
-            //selects method relevant to selection
-            switch (choice) {
-                case 1 -> addMoney();
-                case 2 -> changeBetAmount();
-                case 3 -> play();
-                case 4 -> flag = cashOut();
-                default -> System.out.print("\nInvalid Choice, ");
+        //selects method relevant to selection
+        switch (choice) {
+            case 1 -> {
+                double[] changeMoney = addMoney(scan, playerBalance, machineBalance);
+                playerBalance = changeMoney[0];
+                machineBalance = changeMoney[1];
             }
+            case 2 -> betAmount = changeBetAmount(scan, playerBalance, machineBalance, betAmount);
+            case 3 -> {
+                double[] returnValues = play(scan, playerBalance, machineBalance, betAmount, winnings);
+                playerBalance = returnValues[0];
+                machineBalance = returnValues[1];
+                betAmount = returnValues[2];
+                winnings = returnValues[3];
+            }
+            case 4 -> keepGoing = cashOut(winnings, machineBalance);
+            default -> System.out.print("\nInvalid Choice, ");
         }
+        return keepGoing;
+
     }
 
     /**
      * Allows user to add money to the machine
+     *
+     * @param scan           Takes a scanner for user input
+     * @param playerBalance  takes the player's current balance
+     * @param machineBalance takes the machine's current balance
+     * @return a double[] array consisting of {playerBalance, machineBalance}
      */
-    public static void addMoney() {
+    public static double[] addMoney(Scanner scan, double playerBalance, double machineBalance) {
         boolean flag = true;
         double balance;
         do {
@@ -84,19 +105,26 @@ public class SlotMachine {
             if (balance > playerBalance || balance < 0) {
                 System.out.println("Invalid amount, please change how much you are depositing. " +
                         "(This cannot be over how much money you have or negative)");
-                printStats();
+                printStats(playerBalance, machineBalance);
             } else {
                 machineBalance += balance;
                 playerBalance -= balance;
                 flag = false;
             }
-        }while(flag);
+        } while (flag);
+        return new double[]{playerBalance, machineBalance};
     }
 
     /**
      * Allows user to change bet amount
+     *
+     * @param scan           Takes a scanner for user input
+     * @param playerBalance  takes the player's current balance
+     * @param machineBalance takes the machine's current balance
+     * @param betAmount      takes the bet amount for the user
+     * @return double of new betAmount value;
      */
-    public static void changeBetAmount() {
+    public static double changeBetAmount(Scanner scan, double playerBalance, double machineBalance, double betAmount) {
         //variable declaration
         boolean flag = true;
         double amount;
@@ -111,7 +139,7 @@ public class SlotMachine {
             if (amount > playerBalance) {
                 System.out.println("Invalid bet, please change how much you are betting. " +
                         "(This cannot be over how much money you have)");
-                printStats();
+                printStats(playerBalance, machineBalance);
 
             }
 
@@ -127,36 +155,53 @@ public class SlotMachine {
         //Tells user the bet
         System.out.printf("Your bet has been placed at $ %.2f\n\n", betAmount);
         System.out.println();
-
+        return betAmount;
     }
 
     /**
      * Allows user to play the game
+     *
+     * @param scan           Takes a scanner for user input
+     * @param playerBalance  takes the player's current balance
+     * @param machineBalance takes the machine's current balance
+     * @param betAmount      takes the bet amount for the user
+     * @param winnings       takes the amount of winnings for the player
+     * @return double[] array consisting of {playerBalance, machineBalance, betAmount, winnings}
      */
-    public static void play() {
+    public static double[] play(Scanner scan, double playerBalance, double machineBalance, double betAmount, double winnings) {
         //initializes variables
         final String[] WORDS = {"Computer", "Science", "Java", "Hello", "World", "Professor", "Helsing"};
         String[][] reels = new String[3][3];
+        double[] returnValues = {playerBalance, machineBalance, betAmount, winnings};
 
+
+        return Arrays.copyOf(returnValues, returnValues.length);
     }
 
     /**
      * Displays ending message and Cash Out
+     *
+     * @param winnings       takes the amount of winnings for the player
+     * @param machineBalance takes the machine's current balance
+     * @return boolean value to tell the main slot machine to stop going
      */
-    public static boolean cashOut(){
+    public static boolean cashOut(double winnings, double machineBalance) {
         System.out.println("Thanks for playing the Slot Machine!!");
-        if(winnings > 0){
-            System.out.printf("Congratulations, you have won $%.2f, a total of $%.2f will be returned to you!\n", winnings,machineBalance);
-        } else{
-            System.out.printf("Sorry, you have lost $%.2f, a total of $%.2f will be returned to you!\n", winnings,machineBalance);
+        if (winnings > 0) {
+            System.out.printf("Congratulations, you have won $%.2f, a total of $%.2f will be returned to you!\n", winnings, machineBalance);
+        } else {
+            System.out.printf("Sorry, you have lost $%.2f, a total of $%.2f will be returned to you!\n", winnings, machineBalance);
         }
         return false;
     }
 
     /**
      * Tells user their current balances
+     *
+     * @param playerBalance  takes the player's current balance
+     * @param machineBalance takes the machine's current balance
      */
-    public static void printStats(){
+    public static void printStats(double playerBalance, double machineBalance) {
         System.out.printf("Your balance is now $%.2f and there is $%.2f in the machine)\n", playerBalance, machineBalance);
     }
 }
